c340aae657aa976284f85468bc83a395657d8443
samade747 <samad.e747@gmail.com>
2023-09-13 00:18:23 +0500

    22

--- app.js
+++ app.js
@@ -1,38 +1,39 @@
 function updateClock() {
-    const hourElem = document.getElementById("hours");
-    const minuteElem = document.getElementById("minutes");
-    const secondElem = document.getElementById("seconds");
-    const ampmElem = document.getElementById("ampm");
+    function updateClock() {
+        const hourElem = document.getElementById("hours");
+        const minuteElem = document.getElementById("minutes");
+        const secondElem = document.getElementById("seconds");
+        const ampmElem = document.getElementById("ampm");
 
-    const date = new Date();
-    let hour = date.getHours();
-    let minute = date.getMinutes();
-    let second = date.getSeconds();
-    let ampm = "AM";
+        const date = new Date();
+        let hour = date.getHours();
+        let minute = date.getMinutes();
+        let second = date.getSeconds();
+        let ampm = "AM";
 
-    if (hour >= 12) {
-        ampm = "PM";
+        if (hour >= 12) {
+            ampm = "PM";
+        }
+
+        if (hour > 12) {
+            hour -= 12;
+        }
+
+        hour = hour < 10 ? "0" + hour : hour.toString();
+        minute = minute < 10 ? "0" + minute : minute.toString();
+        second = second < 10 ? "0" + second : second.toString();
+
+        hourElem.textContent = hour;
+        minuteElem.textContent = minute;
+        secondElem.textContent = second;
+        ampmElem.textContent = ampm;
     }
 
-    if (hour > 12) {
-        hour -= 12;
-    }
+    // Call the updateClock function every second
+    setInterval(updateClock, 1000);
 
-    hour = hour < 10 ? "0" + hour : hour;
-    minute = minute < 10 ? "0" + minute : minute;
-    second = second < 10 ? "0" + second : second;
-
-    hourElem.textContent = hour;
-    minuteElem.textContent = minute;
-    secondElem.textContent = second;
-    ampmElem.textContent = ampm;
-}
-
-
-setInterval(updateClock, 1000);
-
-
-updateClock();
+    // Initial call to set the clock when the page loads
+    updateClock();
 
 
 
